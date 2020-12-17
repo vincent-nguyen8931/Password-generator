310bc60d17d50be0b9523b094253369731400fa4
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2020-12-16 19:21:50 -0800

    Create genAlphabet function

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,20 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function genAlphabet(A, Z) {
+  var alphabet = [], i = A.charCodeAt(0), j = Z.charCodeAt(0);
+  for (; i <= j; i++) {
+    alphabet.push(String.fromCharCode(i));
+  }
+  return alphabet;
+}
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(numbers)
+console.log(genAlphabet("a", "z"));
+console.log(genAlphabet("A", "Z"))
+console.log(genAlphabet("b", "x"))
+console.log(genNumbers("0", "9"))
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
