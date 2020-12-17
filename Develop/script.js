d02137a052e990e4f2a20dd0ba811dc1b5794822
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2020-12-16 19:42:37 -0800

    Create array of special characters

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// Generate alphabet by assigning the first and last characters of the alphabet to different variables. The for loop will iterate pushing each character of the alphabet onto the array until it reaches Z.
 function genAlphabet(A, Z) {
   var alphabet = [], i = A.charCodeAt(0), j = Z.charCodeAt(0);
   for (; i <= j; i++) {
@@ -19,6 +20,9 @@ function genAlphabet(A, Z) {
 
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+
+var str = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var special = str.split("")
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
