046896b52ce0130f617ff4672c74abd15b572c5f
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2020-12-17 21:33:58 -0800

    Add error checking to genPasswordChoices function

--- script.js
+++ script.js
@@ -4,13 +4,12 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   checkInput()
-  var pwChars = genPasswordChoices()
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
  
-
+  var pwChars = genPasswordChoices()
   passwordText.value = password;
-
+  console.log(pwChars)
 }
 // Generate alphabet by assigning the first and last characters of the alphabet to different variables. These variables use charCodeAt(0) to set their integer based on the UTF-16 code index. The for loop will iterate pushing each character of the alphabet onto the array as a string until it adds Z.
 function genAlphabet(A, Z) {
@@ -25,52 +24,66 @@ function genAlphabet(A, Z) {
 function genPasswordChoices() {
   // array to hold user's choice of password character types 
   var storeChoices = []
-
   // check to see if user wants upper case letters
   var lettersUp = confirm("Do you want upper case letters in your password?")
-  // if yes, use this variable
-  if (lettersUp === true) {
-    var alphabetUpper = genAlphabet("A", "Z")
-    // push uppercase alphabet onto storeChoices array
-    for (i = 0; i < alphabetUpper.length; i++) {
-    storeChoices.push(alphabetUpper[i])
-    } 
-  } 
-
   // check to see if user wants lower case letters
   var lettersLow = confirm("Do you want lower case letters in your password?")
-  //  if yes, use this variable
-  if (lettersLow === true) {
-    var alphabetLower = genAlphabet("a", "z")
-    // push lowercase alphabet onto storeChoices array
-    for (i = 0; i < alphabetLower.length; i++) {
-    storeChoices.push(alphabetLower[i])
-    } 
-  }
-     
   // check to see if user wants numbers
   var num = confirm("Do you want numbers in your password?")
-  //  if yes, use this variable
-  if (num === true) {
-    var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    // push numbers onto storeChoices array
-    for (i = 0; i < numbers.length; i++) {
-    storeChoices.push(numbers[i])
-    } 
-  }
-
   // check to see if user wants special characters
   var sChar = confirm("Do you want special characters in your password?")
-  //  if yes, use this variable
-  if (num === true) {
-    var str = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-    var special = str.split("")
-    // push special characters onto storeChoices array
-    for (i = 0; i < special.length; i++) {
-    storeChoices.push(special[i])
-    } 
+  //  checks to ensure at least one character type is chosen.
+  while (lettersUp === false &&
+         lettersLow === false &&
+         num === false &&
+         sChar === false ) {
+    alert("Please choose a character type for your password.")  
+    // check to see if user wants upper case letters
+    var lettersUp = confirm("Do you want upper case letters in your password?")
+    // check to see if user wants lower case letters
+    var lettersLow = confirm("Do you want lower case letters in your password?")
+    // check to see if user wants numbers
+    var num = confirm("Do you want numbers in your password?")
+    // check to see if user wants special characters
+    var sChar = confirm("Do you want special characters in your password?")
   }
-}
+     // if lettersUp is true, use this variable
+     if (lettersUp === true) {
+      var alphabetUpper = genAlphabet("A", "Z")
+      // push uppercase alphabet onto storeChoices array
+      for (i = 0; i < alphabetUpper.length; i++) {
+      storeChoices.push(alphabetUpper[i])
+      } 
+    } 
+
+    //  if lettersLow is true, use this variable
+    if (lettersLow === true) {
+      var alphabetLower = genAlphabet("a", "z")
+      // push lowercase alphabet onto storeChoices array
+      for (i = 0; i < alphabetLower.length; i++) {
+      storeChoices.push(alphabetLower[i])
+      } 
+    }
+    
+    //  if num is true, use this variable
+    if (num === true) {
+      var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      // push numbers onto storeChoices array
+      for (i = 0; i < numbers.length; i++) {
+      storeChoices.push(numbers[i])
+      } 
+    }
+
+    //  if sChar is true, use this variable
+    if (sChar === true) {
+      var str = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+      var special = str.split("")
+      // push special characters onto storeChoices array
+      for (i = 0; i < special.length; i++) {
+      storeChoices.push(special[i])
+      } 
+    } return storeChoices
+} 
 
 // Checks the user's input for proper character length. 
 function checkInput() {
