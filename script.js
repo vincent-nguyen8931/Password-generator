0cffce214481ce8e0260447658d00566a2243a79
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-01-11 20:12:08 -0800

    Add checkboxes, and modal error windows

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+$("#closeModal1").click(function () {
+  $("#charAmountTooLow").removeClass("is-active")
+})
+
+$("#closeModal2").click(function () {
+$("#charAmountTooHigh").removeClass("is-active")
+})
+
 // Write password to the #password input
 function writePassword() {
   var charAmount = checkInput()
   if (charAmount != null) {
-  var pwChars = genPasswordChoices()
-  var password = generatePassword(pwChars, charAmount);
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+    var pwChars = genPasswordChoices()
+    var password = generatePassword(pwChars, charAmount);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
   }
 }
 
@@ -26,69 +34,70 @@ function genPasswordChoices() {
   // array to hold user's choice of password character types 
   var storeChoices = []
   // check to see if user wants upper case letters
-  var lettersUp = confirm("Do you want upper case letters in your password?")
+  var lettersUp = $("input[name='yesUpperAlphabet']:checked").val();
   // check to see if user wants lower case letters
-  var lettersLow = confirm("Do you want lower case letters in your password?")
+  var lettersLow = $("input[name='yesLowerAlphabet']:checked").val();
   // check to see if user wants numbers
-  var num = confirm("Do you want numbers in your password?")
+  var num = $("input[name='yesNumber']:checked").val();
   // check to see if user wants special characters
-  var sChar = confirm("Do you want special characters in your password?")
+  var sChar = $("input[name='yesSymbols']:checked").val();
   //  checks to ensure at least one character type is chosen.
-  while (lettersUp === false &&
-         lettersLow === false &&
-         num === false &&
-         sChar === false ) {
-    alert("Please choose a character type for your password.")  
-    // check to see if user wants upper case letters
-    var lettersUp = confirm("Do you want upper case letters in your password?")
+  while (lettersUp == false &&
+    lettersLow == false &&
+    num == false &&
+    sChar == false) {
+    alert("Please choose a character type for your password.")
+     // check to see if user wants upper case letters
+    var lettersUp = $("input[name='yesUpperAlphabet']:checked").val();
     // check to see if user wants lower case letters
-    var lettersLow = confirm("Do you want lower case letters in your password?")
+    var lettersLow = $("input[name='yesLowerAlphabet']:checked").val();
     // check to see if user wants numbers
-    var num = confirm("Do you want numbers in your password?")
+    var num = $("input[name='yesNumber']:checked").val();
     // check to see if user wants special characters
-    var sChar = confirm("Do you want special characters in your password?")
+    var sChar = $("input[name='yesSymbols']:checked").val();
+    //  checks to ensure at least one character type is chosen.
   }
-     // if lettersUp is true, use this variable
-     if (lettersUp === true) {
-      var alphabetUpper = genAlphabet("A", "Z")
-      // push uppercase alphabet onto storeChoices array
-      for (i = 0; i < alphabetUpper.length; i++) {
+  // if lettersUp is true, use this variable
+  if (lettersUp === "checked") {
+    var alphabetUpper = genAlphabet("A", "Z")
+    // push uppercase alphabet onto storeChoices array
+    for (i = 0; i < alphabetUpper.length; i++) {
       storeChoices.push(alphabetUpper[i])
-      } 
-    } 
+    }
+  }
 
-    //  if lettersLow is true, use this variable
-    if (lettersLow === true) {
-      var alphabetLower = genAlphabet("a", "z")
-      // push lowercase alphabet onto storeChoices array
-      for (i = 0; i < alphabetLower.length; i++) {
+  //  if lettersLow is true, use this variable
+  if (lettersLow === "checked") {
+    var alphabetLower = genAlphabet("a", "z")
+    // push lowercase alphabet onto storeChoices array
+    for (i = 0; i < alphabetLower.length; i++) {
       storeChoices.push(alphabetLower[i])
-      } 
     }
-    
-    //  if num is true, use this variable
-    if (num === true) {
-      var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-      // push numbers onto storeChoices array
-      for (i = 0; i < numbers.length; i++) {
-      storeChoices.push(numbers[i])
-      } 
-    }
+  }
 
-    //  if sChar is true, use this variable
-    if (sChar === true) {
-      var str = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-      var special = str.split("")
-      // push special characters onto storeChoices array
-      for (i = 0; i < special.length; i++) {
+  //  if num is true, use this variable
+  if (num === "checked") {
+    var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    // push numbers onto storeChoices array
+    for (i = 0; i < numbers.length; i++) {
+      storeChoices.push(numbers[i])
+    }
+  }
+
+  //  if sChar is true, use this variable
+  if (sChar === "checked") {
+    var str = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    var special = str.split("")
+    // push special characters onto storeChoices array
+    for (i = 0; i < special.length; i++) {
       storeChoices.push(special[i])
-      } 
-    } return storeChoices
-} 
+    }
+  } return storeChoices
+}
 
 // Checks the user's input for proper character length. 
 function checkInput() {
-  var charLimit = prompt("Please choose amount of characters for password.")
+  var charLimit = $("#userInput").val()
   var charSplit = charLimit.split("")
   var numCheck = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
@@ -97,31 +106,32 @@ function checkInput() {
     if (numCheck.includes(charSplit[i]) !== true) {
       alert("Please use a number between 8 and 128.")
       return null
-    } else { 
-      
+    } else {
+
       while (charLimit < 8 || charLimit > 128) {
-      
+
         // if characters are less than 8
         if (charLimit < 8) {
-          alert("Password needs minimum of 8 characters.")
-        } 
-      
+          $("#charAmountTooLow").addClass("is-active");
+        }
+     
         // if characters are greater than 128
         if (charLimit > 128) {
-        alert("Password has maximum of 128 characters.")
+          $("#charAmountTooHigh").addClass("is-active");
         }
+     
         // repeats checking for proper user's input
         return null
-      }  
-    } 
+      }
+    }
   } return charLimit
 }
-     
+
 // Use characters selected for password and amount of characters desired to make the password. 
 function generatePassword(pwChars, charAmount) {
   var pwGenConvert = []
   // choose random number based on characters selected and amount of characters chosen
-  for (i = 0; i < charAmount; i++){
+  for (i = 0; i < charAmount; i++) {
     indexGen = Math.floor(Math.random(pwChars) * pwChars.length)
     // pushes character from pwChars based on index generated by indexGen into pwGenConvert
     pwGenConvert.push(pwChars[indexGen])
